9e7ec78f031860172d5fefc6d37ce94e68a87d23
andy <andy.softdev@bluewin.ch>
2013-12-04 15:14:49 +0100

    New tests added

--- src/ht4o.Test/TestColumnBinding.cs
+++ src/ht4o.Test/TestColumnBinding.cs
@@ -20,6 +20,8 @@
  */
 namespace Hypertable.Persistence.Test
 {
+    using System.Linq;
+
     using Hypertable;
     using Hypertable.Persistence.Bindings;
     using Hypertable.Persistence.Test.TestColumnBindingTypes;
@@ -147,6 +149,60 @@ namespace Hypertable.Persistence.Test
 
             #endregion
         }
+
+        [Entity("TestEntityManager")]
+        internal class EntityC
+        {
+            #region Constructors and Destructors
+
+            public EntityC() {
+                this.Name = Guid.NewGuid().ToString();
+            }
+
+            #endregion
+
+            #region Public Properties
+
+            public EntityA A { get; set; }
+
+            public Key Key { get; set; }
+
+            public string Name { get; set; }
+
+            #endregion
+
+            #region Public Methods and Operators
+
+            public override bool Equals(object o)
+            {
+                if (ReferenceEquals(this, o))
+                {
+                    return true;
+                }
+
+                if (!(o is EntityC))
+                {
+                    return false;
+                }
+
+                return string.Equals(this.Name, (o as EntityC).Name) && (this.A == null ? (o as EntityC).A == null : this.A.Equals((o as EntityC).A));
+            }
+
+            public override int GetHashCode() {
+                return this.Name.GetHashCode() ^ (this.A != null ? this.A.GetHashCode() : 0);
+            }
+
+            #endregion
+        }
+
+        [Entity("TestEntityManager")]
+        internal class EntityC1 : EntityC
+        {
+        }
+
+        [Entity("TestEntityManager")]
+        internal class EntityC2 : EntityC {
+        }
     }
 
     /// <summary>
@@ -168,9 +224,13 @@ namespace Hypertable.Persistence.Test
             Assert.IsFalse(bindingContext.StrictExplicitKeyBinding);
             Assert.IsFalse(bindingContext.StrictExplicitTableBinding);
 
+            bindingContext.StrictExplicitColumnBinding = true;
+
             Assert.IsTrue(bindingContext.RegisterColumnBinding(typeof(EntityA), new ColumnBindingEntityA()));
             Assert.IsFalse(bindingContext.RegisterColumnBinding(typeof(EntityA), new ColumnBindingEntityA()));
             Assert.IsTrue(bindingContext.RegisterColumnBinding(typeof(EntityB), new ColumnBinding("b", "qb")));
+            Assert.IsTrue(bindingContext.RegisterColumnBinding(typeof(EntityC1), new ColumnBinding("c", "1")));
+            Assert.IsTrue(bindingContext.RegisterColumnBinding(typeof(EntityC2), new ColumnBinding("c", "2")));
 
             var eb1 = new EntityB();
             TestBase.TestSerialization(eb1);
@@ -178,10 +238,18 @@ namespace Hypertable.Persistence.Test
             var eb2 = new EntityB { A = new EntityA() };
             TestBase.TestSerialization(eb2);
 
+            var ec1 = new EntityC1 { A = new EntityA() };
+            TestBase.TestSerialization(ec1);
+
+            var ec2 = new EntityC2 { A = new EntityA() };
+            TestBase.TestSerialization(ec2);
+
             using (var em = Emf.CreateEntityManager(bindingContext))
             {
                 Assert.IsTrue(em.IsTypeDeclared<EntityA>());
                 Assert.IsTrue(em.IsTypeDeclared<EntityB>());
+                Assert.IsTrue(em.IsTypeDeclared<EntityC1>());
+                Assert.IsTrue(em.IsTypeDeclared<EntityC2>());
 
                 em.Configuration.Binding.StrictExplicitColumnBinding = true;
 
@@ -201,6 +269,18 @@ namespace Hypertable.Persistence.Test
                 Assert.IsFalse(string.IsNullOrEmpty(eb2.A.Key.Row));
                 Assert.AreEqual("a", eb2.A.Key.ColumnFamily);
                 Assert.AreEqual("qa", eb2.A.Key.ColumnQualifier);
+
+                em.Persist(ec1);
+                Assert.IsNotNull(ec1.Key);
+                Assert.IsFalse(string.IsNullOrEmpty(ec1.Key.Row));
+                Assert.AreEqual("c", ec1.Key.ColumnFamily);
+                Assert.AreEqual("1", ec1.Key.ColumnQualifier);
+
+                em.Persist(ec2);
+                Assert.IsNotNull(ec2.Key);
+                Assert.IsFalse(string.IsNullOrEmpty(ec2.Key.Row));
+                Assert.AreEqual("c", ec2.Key.ColumnFamily);
+                Assert.AreEqual("2", ec2.Key.ColumnQualifier);
             }
 
             using (var em = Emf.CreateEntityManager(bindingContext))
@@ -213,11 +293,24 @@ namespace Hypertable.Persistence.Test
 
                 var _eb2 = em.Find<EntityB>(eb2.Key);
                 Assert.AreEqual(eb2, _eb2);
+
+                var _ec1 = em.Find<EntityC1>(ec1.Key);
+                Assert.AreEqual(ec1, _ec1);
+
+                var _ec2 = em.Find<EntityC2>(ec2.Key);
+                Assert.AreEqual(ec2, _ec2);
+
+                var ecl = em.Fetch<EntityC>().ToList();
+                Assert.AreEqual(2, ecl.Count);
+                Assert.IsTrue(ecl.Contains(_ec1));
+                Assert.IsTrue(ecl.Contains(_ec2));
             }
 
             Assert.IsTrue(bindingContext.UnregisterColumnBinding(typeof(EntityA)));
             Assert.IsFalse(bindingContext.UnregisterColumnBinding(typeof(EntityA)));
             Assert.IsTrue(bindingContext.UnregisterColumnBinding(typeof(EntityB)));
+            Assert.IsTrue(bindingContext.UnregisterColumnBinding(typeof(EntityC1)));
+            Assert.IsTrue(bindingContext.UnregisterColumnBinding(typeof(EntityC2)));
         }
 
         #endregion
